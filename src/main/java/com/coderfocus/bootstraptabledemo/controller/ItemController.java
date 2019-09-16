2fc4fc8f3a129b6b94509b632a4886a57a17c490
coderfocus <songwenjie830@gmail.com>
2019-09-16 16:33:55 +0800

    feat(boostrap-table): 新增表格分页查询

--- src/main/java/com/coderfocus/bootstraptabledemo/controller/ItemController.java
+++ src/main/java/com/coderfocus/bootstraptabledemo/controller/ItemController.java
@@ -4,22 +4,30 @@ import com.coderfocus.bootstraptabledemo.dto.Page;
 import com.coderfocus.bootstraptabledemo.entity.Item;
 import com.coderfocus.bootstraptabledemo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
 
 @RequestMapping("/item")
-@RestController
+@Controller
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
+    @ResponseBody
     @GetMapping("/page")
-    public Page<Item> pageQuery(Page page){
+    public Page<Item> pageQuery(@RequestParam(defaultValue = "0") Integer offset, @RequestParam(defaultValue = "10")Integer limit){
+        Page page = new Page();
+        page.setOffset(offset);
+        page.setLimit(limit);
         return itemService.pageQuery(page);
+    }
+
+    @GetMapping("/pageTable")
+    public String page(){
+        return "/pageTable";
     }
 }
